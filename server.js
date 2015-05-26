aed87377793cf633cb5c93eb72cba88b1b681087
NathanBuckley <nathan@pinarylabs.com>
2015-05-26 16:48:33 +0100

    updating app structure

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var express = require('express'),
 app = express(),
 io = require('socket.io'),
 fs = require('fs'),
+path = require('path'),
 bodyParser = require('body-parser'),
 port = process.env.PORT || 1337;
 
@@ -18,12 +19,21 @@ app.use(bodyParser.json()); //support json encoded bodies
 app.use(express.static(__dirname + '/public'));
 
 
+// FORM ROUTES ---------------------------------------------
 
+app.post('/', function (req, res) {
+  res.sendFile(path.join(__dirname + 'public/index.html'));
+});
+
+app.post('/perdetails', function (req, res) {
+  res.send(req.body);
+    console.log(req.body);
+});
 
 
 // START THE SERVER ---------------------------------------------
 
 var server = app.listen(port, function(){
   var host = server.address().address;
-  //console.log('listening on: '+ host +':'+ port);
+  console.log('listening on: ' + host + ':' + port);
 });
