cdbfc1e4f9a2940b263c5d55f6ac581d563db2bc
NathanBuckley <nathan@pinarylabs.com>
2015-05-26 06:58:12 +0100

    setting up dash for express backend

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 /*
 Express server for backend of dashboard
 */
+
+var express = require('express'),
+app = express(),
+io = require('socket.io'),
+fs = require('fs'),
+bodyParser = require('body-parser'),
+port = process.env.PORT || 1337;
+
+
+
+//config ---------------------------------------------------
+
+app.use(bodyParser.urlencoded({ extended: true })); //support encoded bodies (x-www-form-urlencoded)
+app.use(bodyParser.json()); //support json encoded bodies
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+
+// START THE SERVER ---------------------------------------------
+
+var server = app.listen(port, function(){
+  var host = server.address().address;
+  //console.log('listening on: '+ host +':'+ port);
+});
