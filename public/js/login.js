15e0071bdf516645431bdc48fe3ac2eac40d97d7
NathanBuckley <nathan@pinarylabs.com>
2015-06-02 13:19:02 +0100

    added validation function

--- public/js/login.js
+++ public/js/login.js
@@ -9,3 +9,18 @@ $(document).ready(function(){
 //-------------------------------------------------------
 
 //login validation
+
+
+function validation() {
+  var giturl = document.getElementById("giturl").value;
+  console.log(giturl);
+  var apikey = document.getElementById("apikey").value;
+  console.log(apikey);
+  if (giturl === '' || apikey === '') {
+    console.log("Please fill all fields...!!!!!!");
+    return false;
+  }
+  else {
+    return true;
+  }
+}
