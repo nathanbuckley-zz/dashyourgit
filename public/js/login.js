fce7153d56f135d39b116a380ba53fb505b304d8
NathanBuckley <nathan@pinarylabs.com>
2015-06-10 07:09:09 +0100

    added validation for org and repo forms

--- public/js/login.js
+++ public/js/login.js
@@ -24,3 +24,28 @@ function validation() {
     return true;
   }
 }
+
+
+function validationOrg() {
+  var orgUrl = document.getElementById("orgUrl").value;
+  var apikey = document.getElementById("apikeyOrg").value;
+  if (orgUrl === '' || apikey === '') {
+    console.log("Please fill all fields...!!!!!!");
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+function validationRepo() {
+  var repoUrl = document.getElementById("repoUrl").value;
+  var apikey = document.getElementById("apikeyrepo").value;
+  if (repoUrl === '' || apikey === '') {
+    console.log("Please fill all fields...!!!!!!");
+    return false;
+  }
+  else {
+    return true;
+  }
+}
