fa44ac4701dcfbb1790cf9e3075caa1deb402072
NathanBuckley <nathan@pinarylabs.com>
2015-08-10 07:36:22 +0100

    added user info collector

--- DataCollection/collection.js
+++ DataCollection/collection.js
@@ -14,4 +14,30 @@ var collect = function(url){
   });
 }
 
+var userInf = function(un){
+  var userDeetsObj = {
+    hostname: 'api.github.com',
+    path: 'users/' + conf.gitUrl,
+    headers: {
+      'User-Agent': 'DashYourGit'
+    }
+  }
+  var r = '';
+  https.get(userDeetsObj, function(res){
+    res.on('data',function(data){
+      r += data.toString('utf8');
+      //console.log(r);
+    });
+    res.on('end', function(){
+      return r;
+      console.log(JSON.stringify(r));
+      fs.writeFile('userinfo.json', r, function(err){
+      if (err) throw err;
+      console.log('user Information Saved');
+      console.log(r);
+    });
+  });
+}
+
 exports.collect = collect;
+exports.userInf = userInf;
